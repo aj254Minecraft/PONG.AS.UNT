d986f2b8e4bd4f2cbc372f34691633febe79eb6b
aj254Minecraft <AidanSalas@my.unt.edu>
2024-05-28 21:17:52 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -49,7 +49,7 @@ soundArray.push(wallSound); //add sound to list of sounds
 export var paddleSound = new soundFile("e.mp3");
 soundArray.push(paddleSound);
 
-export var scoreSound = new soundFile("e.mp3");
+export var scoreSound = new soundFile("j.mp3");
 soundArray.push(scoreSound);
 
 export var ambientSound = new soundFile("e.mp3");
